f71d1f8e31685e47fde397337f6caf5460e08fb3
Andrew Lisowski <lisowski54@gmail.com>
2019-05-03 18:12:30 -0700

    :memo: Update Documentation [skip ci]

--- image-auto.gif.js
+++ image-auto.gif.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{100:function(p,a,n){p.exports=n.p+"c2288ac837a6ba66db760767fe4a1591.gif"}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{102:function(p,a,n){p.exports=n.p+"c2288ac837a6ba66db760767fe4a1591.gif"}}]);
 //# sourceMappingURL=image-auto.gif.js.map
